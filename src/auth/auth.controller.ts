c494b63ba629a8bc37bfc1d5f1f651d280181023
Keshav Jindal <keshav.j@KESHAV-583-GTG.bbrouter>
2023-01-11 19:21:31 +0530

    Initial Commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Request } from "express";
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    signup(@Body() dto: any){
+        console.log({
+            dto: dto
+        });
+        
+        return this.authService.signup()
+    }
+
+    @Post('signin')
+    signin(){
+        return this.authService.signin()
+    }
+}
